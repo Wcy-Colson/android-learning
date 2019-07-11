0b188994a8fcfeb05a1d5bae4348620bfed62f92
wcyCoulson <43095445+wcyCoulson@users.noreply.github.com>
2019-07-11 19:34:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/homework1/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.homework1;
+
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edittext1;
+    private EditText edittext2;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//       FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        edittext1=(EditText) findViewById(R.id.editText1);
+        edittext2=(EditText) findViewById(R.id.editText2);
+        button = (Button) findViewById(R.id.button);
+        //为button按钮注册监听器，并通过匿名内部类实现
+        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+            public void onClick(View v) {
+                Log.v("phonenumber", String.valueOf(edittext1.getText()));
+                Log.v("password",String.valueOf(edittext2.getText()));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
